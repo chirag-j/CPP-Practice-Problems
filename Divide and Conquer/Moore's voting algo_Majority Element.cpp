794dc383df386349fe5a83ca0cd9421b7f3de495
chirag-j <chiragjaiswal8@gmail.com>
2021-06-09 11:42:07 +0530

    NEW COMMIT

---
+++ Divide and Conquer/Moore's voting algo_Majority Element.cpp
@@ -0,0 +1,17 @@
+int majorityElement(vector<int>& nums) {
+    int ansIndex = 0;
+    int count = 1;
+    for(int i=1; i<nums.size(); i++){
+        if(nums[i]==nums[ansIndex]){
+            count++;
+        }
+        else{
+            count--;
+        }
+        if(count == 0){
+            ansIndex = i;
+            count = 1;
+        }
+    }
+    return nums[ansIndex];
+}
